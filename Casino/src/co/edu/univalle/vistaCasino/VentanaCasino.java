e55af4b7267fb5f50fd0b2fad15d5db75f355680
Kaos <brayan.andres.sanchez@correounivalle.edu.co>
2022-05-08 16:32:07 -0500

    Cambio GridBagLayout

--- Casino/src/co/edu/univalle/vistaCasino/VentanaCasino.java
+++ Casino/src/co/edu/univalle/vistaCasino/VentanaCasino.java
@@ -3,12 +3,15 @@ package co.edu.univalle.vistaCasino;
 import co.edu.univalle.sistema.Jugador;
 import co.edu.univalle.sistema.Maquina;
 import co.edu.univalle.sistema.Sistema;
-import java.awt.BorderLayout;
+
 import java.awt.Container;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 import java.awt.event.*;
 import java.util.Random;
 import javax.swing.*;
+import javax.swing.SwingUtilities;
 
 /**
  *
@@ -19,27 +22,29 @@ public class VentanaCasino extends JFrame {
     //Declaracion de variables                    
     private Sistema juegoCasino = new Sistema();
     Random aleatorio = new Random();
+    GridBagConstraints constraints = new GridBagConstraints();
 
-    private javax.swing.JLabel lblTitulo;
-    private javax.swing.JLabel lblMensaje;
-    private javax.swing.JLabel lbldado1;
-    private javax.swing.JLabel lbldado2;
-    private javax.swing.JButton btnLanzar;
-    private javax.swing.JTextField txtResultado;
+    // Elementos para el lanzamiento de dados
+    private JLabel lblTitulo;
+    private JLabel lbldado1;
+    private JLabel lbldado2;
+    private JButton btnLanzar;
+    private JTextField txtResultado;
     private Container contenedorPpal;
 
-    private javax.swing.JLabel lblModoJuego;
-    private javax.swing.JTextField txtModoJuego;
-    private javax.swing.JLabel lblNombre1;
-    private javax.swing.JTextField txtNombre1;
-    private javax.swing.JLabel lblNombre2;
-    private javax.swing.JTextField txtNombre2;
-    private javax.swing.JLabel lblNumeroLanzamientos;
-    private javax.swing.JTextField txtNumeroLanzamientos;
-    private javax.swing.JLabel lblRondas;
-    private javax.swing.JTextField txtRondas;
-    private javax.swing.JButton btnSeguirResumen;
-    private javax.swing.JButton btnReiniciarResumen;
+    private JLabel lblModoJuego;
+    private JTextField txtModoJuego;
+    private JLabel lblNombre1;
+    private JTextField txtNombre1;
+    private JLabel lblNombre2;
+    private JTextField txtNombre2;
+    private JLabel lblNumeroLanzamientos;
+    private JTextField txtNumeroLanzamientos;
+    private JLabel lblRondas;
+    private JTextField txtRondas;
+    private JButton btnSeguirResumen;
+    private JButton btnReiniciarResumen;
+
 
     /**
      * Creamos una ventana para casino
@@ -57,6 +62,13 @@ public class VentanaCasino extends JFrame {
         setSize(600, 500); //Dimensiones del JFrame
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Cerrar al salir
         setVisible(true); //Mostrar JFrame
+    }
+
+    private void setConstraints(int x, int y, int hx, int hy){
+        constraints.gridx = x;
+        constraints.gridy = y;
+        constraints.gridwidth = hx;
+        constraints.gridheight = hy;
     }
 
     public void elegirModoDeJuego() {
@@ -123,7 +135,6 @@ public class VentanaCasino extends JFrame {
 
             } else {
                 boolean estado = true;
-
                 int rondas = 0;
                 try {
                     rondas = Integer.parseInt(numeroDeRondas);
@@ -259,34 +270,39 @@ public class VentanaCasino extends JFrame {
         lbldado1 = new javax.swing.JLabel();
         lbldado2 = new javax.swing.JLabel();
         txtResultado = new javax.swing.JTextField();
-
         /*
+        contenedorPpal = getContentPane();        
+        contenedorPpal.setLayout(new GridBagLayout());
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
         lblTitulo.setFont(new java.awt.Font("Perpetua Titling MT", 0, 24)); // NOI18N
-        lblTitulo.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        lblTitulo.setText("CASINO UNVIALLE");
+        lblTitulo.setHorizontalAlignment(JLabel.CENTER);
+        lblTitulo.setText("CASINO UNIVALLE");
+        setConstraints(0,0,2,1);
+        contenedorPpal.add(lblTitulo, constraints);
+
+        lbldado1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/dado2.png"))); // NOI18N
+        setConstraints(0,1,1,1);
+        contenedorPpal.add(lbldado1, constraints);
+
+        lbldado2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/dado4.png"))); // NOI18N
+        setConstraints(1,1,1,1);
+        contenedorPpal.add(lbldado2, constraints);
+
+        txtResultado.setEditable(false);
+        txtResultado.setFont(new java.awt.Font("Century Gothic", 0, 18)); // NOI18N
+        setConstraints(0,3,3,1);
+        txtResultado.setBorder(javax.swing.BorderFactory.createEmptyBorder());       
+        contenedorPpal.add(txtResultado, constraints);
 
         btnLanzar.setFont(new java.awt.Font("Century Gothic", 0, 18)); // NOI18N
         btnLanzar.setText("Tira los dados");
         btnLanzar.setActionCommand("BTNLANZAR");
         btnLanzar.addActionListener(gestorEventos);
-
-        lbldado1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/dado2.png"))); // NOI18N
-
-        lbldado2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/dado4.png"))); // NOI18N
-
-        txtResultado.setEditable(false);
-        txtResultado.setFont(new java.awt.Font("Century Gothic", 0, 18)); // NOI18N
-        txtResultado.setHorizontalAlignment(javax.swing.JTextField.CENTER);
-
-        contenedorPpal = getContentPane();
-        contenedorPpal.setLayout(new GridLayout(4, 2));
-
-        contenedorPpal.add(lblTitulo);
-        contenedorPpal.add(lbldado1);
-        contenedorPpal.add(lbldado2);
-        contenedorPpal.add(txtResultado);
-        contenedorPpal.add(btnLanzar);
-         */
+        setConstraints(0,4,2,1);
+        contenedorPpal.add(btnLanzar, constraints);
+    */
     }
 
     class EventosInternos implements ActionListener {
@@ -296,8 +312,8 @@ public class VentanaCasino extends JFrame {
             switch (e.getActionCommand()) {
                 case "BTNLANZAR":
                     //Generar el tiro de los dados, usando numeros generados aleatoriamente
-                    int dado1 = aleatorio.nextInt(7);
-                    int dado2 = aleatorio.nextInt(7);
+                    int dado1 = 1 + aleatorio.nextInt(6);
+                    int dado2 = 1 + aleatorio.nextInt(6);  
 
                     //Generamos los iconos a partir de imagenes
                     Icon uno = new javax.swing.ImageIcon(getClass().getResource("/imagenes/dado1.png"));
@@ -309,42 +325,30 @@ public class VentanaCasino extends JFrame {
 
                     //Mostramos los dados al usuario
                     switch (dado1) {
-                        case 1 ->
-                            lbldado1.setIcon(uno);
-                        case 2 ->
-                            lbldado1.setIcon(dos);
-                        case 3 ->
-                            lbldado1.setIcon(tres);
-                        case 4 ->
-                            lbldado1.setIcon(cuatro);
-                        case 5 ->
-                            lbldado1.setIcon(cinco);
-                        case 6 ->
-                            lbldado1.setIcon(seis);
+                        case 1 -> lbldado1.setIcon(uno);
+                        case 2 -> lbldado1.setIcon(dos);
+                        case 3 -> lbldado1.setIcon(tres);
+                        case 4 -> lbldado1.setIcon(cuatro);
+                        case 5 -> lbldado1.setIcon(cinco);
+                        case 6 -> lbldado1.setIcon(seis);
                         default -> {
-                        }
                     }
-
+                    }
+                    
                     switch (dado2) {
-                        case 1 ->
-                            lbldado2.setIcon(uno);
-                        case 2 ->
-                            lbldado2.setIcon(dos);
-                        case 3 ->
-                            lbldado2.setIcon(tres);
-                        case 4 ->
-                            lbldado2.setIcon(cuatro);
-                        case 5 ->
-                            lbldado2.setIcon(cinco);
-                        case 6 ->
-                            lbldado2.setIcon(seis);
+                        case 1 -> lbldado2.setIcon(uno);
+                        case 2 -> lbldado2.setIcon(dos);
+                        case 3 -> lbldado2.setIcon(tres);
+                        case 4 -> lbldado2.setIcon(cuatro);
+                        case 5 -> lbldado2.setIcon(cinco);
+                        case 6 -> lbldado2.setIcon(seis);
                         default -> {
-                        }
                     }
-
+                    }
+                    
                     //Mostramos por texto el resultado del lanzamiento
-                    int resultado = dado1 + dado2;
-                    txtResultado.setText(resultado + "");
+                    int resultado = dado1+dado2;
+                    txtResultado.setText(resultado+"");
 
                     break;
 
@@ -352,6 +356,7 @@ public class VentanaCasino extends JFrame {
                     JOptionPane.showMessageDialog(null, "a");
                     break;
             }
+        SwingUtilities.updateComponentTreeUI(contenedorPpal);
         }
 
     }
